1f7655a07398701797645d8d0d65e9b195691ab6
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-07-30 19:08:01 +0800

    Working version of simple EL pipeline

--- dags/retail_data_pipeline.py
+++ dags/retail_data_pipeline.py
@@ -1,9 +1,18 @@
 from datetime import datetime, timedelta
 
+from utils import local_to_s3
+
 from airflow import DAG
+from airflow.models import Variable
+
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 
+
+# Get exported variables from Airflow (taskbar Admin -> Variables)
+# Add AWS connection as well (taskbar Admin -> Connections)
+BUCKET_NAME = Variable.get("BUCKET_NAME")
 
 # Default arguments for defining the DAG
 default_args = {
@@ -26,7 +35,7 @@ dag = DAG(
     max_active_runs=1,
 )
 
-# Copies PostgresDB data into CSV file
+# Copies PostgresDB data into a CSV file in the temporary folder
 extract_retail_data = PostgresOperator(
     dag=dag,
     task_id="extract_retail_data",
@@ -37,7 +46,20 @@ extract_retail_data = PostgresOperator(
     wait_for_downstream=True,
 )
 
+# Moves CSV file from temp folder to S3 data lake raw folder
+retail_to_datalake_raw = PythonOperator(
+    dag=dag,
+    task_id="retail_to_datalake_raw",
+    python_callable=local_to_s3,
+    op_kwargs={
+        "file_name": "/temp/retail_profiling.csv",
+        "key": "raw/retail/{{ ds }}/retail_profiling.csv",  # `ds` is the Jinja macro for execution date
+        "bucket_name": BUCKET_NAME,
+        "remove_local": True,
+    },
+)
+
 end_of_data_pipeline = DummyOperator(dag=dag, task_id="end_of_data_pipeline")
 
 
-extract_retail_data >> end_of_data_pipeline
+extract_retail_data >> retail_to_datalake_raw >> end_of_data_pipeline
